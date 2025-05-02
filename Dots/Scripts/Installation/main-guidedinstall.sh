5af583337c579fbe1418ae901e0f6946b43fd42d
Geode <174155806+GeodeArc@users.noreply.github.com>
2025-05-03 11:05:41 +1200

    Hotfix - Remove incorrect paths in guidedinstall

--- Dots/Scripts/Installation/main-guidedinstall.sh
+++ Dots/Scripts/Installation/main-guidedinstall.sh
@@ -2,20 +2,8 @@
 
 clear
 
-# 
-# AUR INSTALL
-# 
-./Dots/Scripts/Installation/install-aur.sh
-
-# 
-# FONT INSTALLATION
-# 
-
-./Dots/Scripts/Installation/install-nerdfonts.sh
-./Dots/Scripts/Installation/install-font.sh
-
 #
-# TWEAKS/FINAL INSTALL 
+# DOTFILES OPTIONS + BASE INSTALL 
 #
 
 ./Dots/Scripts/Installation/install-options.sh
