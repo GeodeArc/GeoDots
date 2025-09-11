97586fc0ddb82e8213d89e80207f7c8e3d424ff8
GeodeArc <174155806+GeodeArc@users.noreply.github.com>
2025-09-11 20:42:10 +1200

    swaync color update fix

--- Dots/Scripts/Hyprland/wall.sh
+++ Dots/Scripts/Hyprland/wall.sh
@@ -13,8 +13,8 @@ ln -s $genwal $HOME/Dots/Options/wallpaper
 echo "* { wallpaper: url(\"$genwal\", width); }" > "$HOME/.config/rofi/options/wallpaper.rasi"
 
 wal -q -i $genwal &
-swaync-client -R &
-swaync-client -rs &
+swaync-client -R
+swaync-client -rs
 $HOME/Dots/Scripts/Waybar/waybar.sh &
 
 notify-send -i preferences-desktop-wallpaper-symbolic "Wallpaper Applied" "New color scheme generated from image:\n$wallname"
