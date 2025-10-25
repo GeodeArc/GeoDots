7ff2e571d444f3b5431529cd4bf4a42586e247c3
GeodeArc <174155806+GeodeArc@users.noreply.github.com>
2025-10-25 19:42:43 +1300

    im really dumb

--- Dots/Scripts/Themes/s-min.sh
+++ Dots/Scripts/Themes/s-min.sh
@@ -7,6 +7,8 @@ echo "minimal" > $HOME/Dots/Options/style
 cp -a $HOME/.config/waybar/minimal/$theme/. $HOME/.config/waybar/
 cp -a $HOME/.config/swaync/minimal/$theme/. $HOME/.config/swaync/
 cp -a $HOME/.config/rofi/minimal/$theme/config.rasi $HOME/.config/rofi/
+cp -a $HOME/.config/hypr/themes/minimal/hyprland.conf $HOME/.config/hypr/
+cp -a $HOME/.config/hypr/themes/minimal/hyprlock.conf $HOME/.config/hypr/
 
 sleep 0.5 
 
