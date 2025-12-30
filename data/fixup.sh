fc6d7d94b526e6f2520e789d4fc5a02b41a3f877
Geode <174155806+GeodeArc@users.noreply.github.com>
2025-12-30 21:06:19 +1300

    fixup.sh for post install fixes (if required)

---
+++ data/fixup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Fixes for 0.1.1 > 0.1.2x
+theme="$(cat $HOME/Dots/Options/theme)"
+style="$(cat $HOME/Dots/Options/style)"
+
+if [[ ! -f "$HOME/.config/hypr/theme.conf" ]]; then
+    cp -r $HOME/.config/hypr/themes/$style/theme.conf $HOME/.config/hypr/theme.conf
+    hyprctl reload
+    clear
+fi
+
+if [[ ! -f "$HOME/.config/swayosd/style.css" ]]; then
+    cp -a "$HOME/.config/swayosd/$style/$theme.css" "$HOME/.config/swayosd/style.css"
+    pkill swayosd
+    setsid swayosd-server &> /dev/null &
+    clear
+fi
