313dfa9b1dc4fc27ba1f9329688155c01019994f
GeodeArc <174155806+GeodeArc@users.noreply.github.com>
2026-06-07 13:44:04 +1200

    remove theme.conf check

--- Dots/Scripts/fixup.sh
+++ Dots/Scripts/fixup.sh
@@ -6,12 +6,6 @@ aurhelper="$(cat $HOME/Dots/Options/aurhelper)"
 theme="$(cat $HOME/Dots/Options/theme)"
 style="$(cat $HOME/Dots/Options/style)"
 
-if [[ ! -f "$HOME/.config/hypr/theme.conf" ]]; then
-    cp -r $HOME/.config/hypr/themes/$style/theme.conf $HOME/.config/hypr/theme.conf
-    hyprctl reload
-    clear
-fi
-
 if [[ ! -f "$HOME/.config/swayosd/style.css" ]]; then
     cp -a "$HOME/.config/swayosd/$style/$theme.css" "$HOME/.config/swayosd/style.css"
     pkill swayosd
