3e0cab73e8c61bcfad2d6a187b5c72b3a557a8e3
GeodeArc <174155806+GeodeArc@users.noreply.github.com>
2026-08-24 21:38:58 +1200

    this works better v2

--- Dots/Scripts/Settings/dotsupgrade.sh
+++ Dots/Scripts/Settings/dotsupgrade.sh
@@ -9,12 +9,6 @@ if [[ $curver != $newver ]]; then
     echo "Press ENTER to continue "
     read -p " ■ "
     clear
-    echo "Downloading latest package lists"
-    curl -o /tmp/pkg-pacman -s https://geodearc.com/GeoDots/data/pkg-pacman
-    curl -o /tmp/pkg-aurs -s https://geodearc.com/GeoDots/data/pkg-aurs
-    curl -o /tmp/pkg-gtk -s https://geodearc.com/GeoDots/data/pkg-gtk
-    curl -o /tmp/pkg-qt -s https://geodearc.com/GeoDots/data/pkg-qt
-
     echo "Downloading upgrade script from github"
     curl -o /tmp/dots-upgrade.sh -s https://geodearc.com/dots-upgrade.sh
     chmod +x /tmp/dots-upgrade.sh
